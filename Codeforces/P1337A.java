c26a8aa50c289e866633d83ecb393d3f5b31b1df
geyaareddyj <geyaareddy.j@gmail.com>
2026-05-25 15:05:33 +0530

    Add files via upload

---
+++ Codeforces/P1337A.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class P1337A {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+    while (t-- > 0) {
+
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
+      int d = sc.nextInt();
+
+      int x = b;
+      int y = c;
+      int z = c;
+
+      System.out.println(x + " " + y + " " + z);
+    }
+  }
+}
